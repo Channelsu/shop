f9ec0a868b7713cdd5da2b4143558f3ec4987e4c
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-25 21:51:52 +0900

    ライブラリのインストール、カートページの土台作成

---
+++ lib/page/cart_page.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class CartPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Column(
+        children: <Widget>[
+          Row(
+            children: <Widget>[
+              
+            ],
+          ),
+        ],
+      ),
+      
+    );
+  }
+}
