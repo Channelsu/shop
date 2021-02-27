4c67bbf7d4276fb735a6b7c30949be9ef24db76a
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-27 09:45:22 +0900

    クラス修正

--- lib/model/cart_item.dart
+++ lib/model/cart_item.dart
@@ -7,5 +7,26 @@ class CartItem {
   final String imgUrl;
   final double price;
 
-  const CartItem({this.id, this.title, this.quantity, this.imgUrl, this.price});
+  const CartItem({
+    @required this.id,
+    @required this.title,
+    @required this.quantity,
+    @required this.imgUrl,
+    @required this.price
+  });
+
+  CartItem copy({
+    String id,
+    String title,
+    int quantity,
+    String imgUrl,
+    double price,
+  }) =>
+      CartItem(
+        id: id ?? this.id,
+        title: title ?? this.title,
+        quantity: quantity ?? this.quantity,
+        imgUrl: imgUrl ?? this.imgUrl,
+        price: price ?? this.price,
+      );
 }
