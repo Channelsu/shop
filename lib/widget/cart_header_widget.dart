f0e4abd97e1683ecde086dada2e62890ac714b96
Channelsu <h_sugimoto0401@yahoo.co.jp>
2021-02-28 18:26:56 +0900

    カートページのヘッダー作成

--- lib/widget/cart_header_widget.dart
+++ lib/widget/cart_header_widget.dart
@@ -13,8 +13,40 @@ class CartHeaderWidget extends StatelessWidget {
       padding: EdgeInsets.symmetric(horizontal: 24, vertical: 4),
       child: Row(
         children: <Widget>[
-          Text('Cart', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),)
+          Text('Cart', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 20),),
+          SizedBox(width: 30,),
+          Expanded(
+            child: SingleChildScrollView(
+              scrollDirection: Axis.horizontal,
+              child: Row(
+                children: provider.items.values.map(buildCardItem).toList(),
+              ),
+            ),
+          ),
+          SizedBox(width: 12,),
+          if(provider.items.isNotEmpty) buildItemCount(context)
         ],
+      ),
+    );
+  }
+
+  Widget buildCardItem(CartItem cardItem) => Padding(
+    padding: EdgeInsets.symmetric(horizontal: 4),
+    child: CircleAvatar(
+      radius: 20,
+      backgroundImage: AssetImage(cardItem.imgUrl),
+    ),
+  );
+
+  Widget buildItemCount(BuildContext context) {
+    final provider = Provider.of<ShopProvider>(context);
+
+    return CircleAvatar(
+      radius: 20,
+      backgroundColor: Theme.of(context).primaryColor,
+      child: Text(
+        provider.itemCount.toString(),
+        style: TextStyle(fontWeight: FontWeight.bold),
       ),
     );
   }
